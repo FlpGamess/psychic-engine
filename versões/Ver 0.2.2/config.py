3ed448cd28fc461319151eb4b6eb6c2cbe3457cc
Pedro <pedroammedeiros@gmail.com>
2025-03-30 15:27:30 -0300

    Update config.py

--- versões/Ver 0.2.2/config.py
+++ versões/Ver 0.2.2/config.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 
 app= Flask(__name__)
 app.secret_key = 'receba'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:jensen947@localhost:5432/gestao_eventos'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:[senha]@localhost:5432/gestao_eventos'
 app.config['UPLOAD_FOLDER'] = 'static/uploads' #pasta que salva as imagens
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg'} #tipos de img permitidas
 
